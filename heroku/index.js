7d269b840fc6f6a050deb2d83bbf0d57039ab07f
jonahokot <47271381+jonahokot@users.noreply.github.com>
2022-10-26 17:56:00 +0300

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -1,3 +1,4 @@
+
 /**
  * Copyright 2016-present, Facebook, Inc.
  * All rights reserved.
@@ -37,6 +38,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 });
 
 app.post('/facebook', function(req, res) {
+  let body = req.body;
   console.log('Facebook request body:', req.body);
 
   if (!req.isXHubValid()) {
@@ -44,6 +46,41 @@ app.post('/facebook', function(req, res) {
     res.sendStatus(401);
     return;
   }
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+     received_updates.unshift(req.body);
+     res.sendStatus(200);
+  } else {
+    // Return a '404 Not Found' if event is not from a WhatsApp API
+    res.sendStatus(404);
+  }
+
 
   console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
