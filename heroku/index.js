9a5a6b5150edeec391a54bb35cedca6016e1ff1b
jonahokot <47271381+jonahokot@users.noreply.github.com>
2022-11-10 14:15:34 +0300

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -15,8 +15,8 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
