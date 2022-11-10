29434f45c593f6de5b69505d2886ed423b6f5076
jonahokot <jonahrokot2@gmail.com>
2022-11-10 15:17:28 +0300

    filter incoming whatsapp events

--- heroku/index.js
+++ heroku/index.js
@@ -1,4 +1,3 @@
-
 /**
  * Copyright 2016-present, Facebook, Inc.
  * All rights reserved.
@@ -7,91 +6,61 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-var express = require('express');
+var express = require("express");
 var app = express();
-var xhub = require('express-x-hub');
+var xhub = require("express-x-hub");
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
 app.use(express.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+var token = process.env.TOKEN || "token";
+let received_updates = [];
 
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send("<pre>" + JSON.stringify(received_updates, null, 2) + "</pre>");
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get("/facebook", function (req, res) {
   if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == token
   ) {
-    res.send(req.query['hub.challenge']);
+    res.send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(400);
   }
 });
 
-app.post('/facebook', function(req, res) {
-  let body = req.body;
-  console.log('Facebook request body:', req.body);
+app.post("/facebook", function (req, res) {
+  console.log("Facebook request body:", req.body);
 
   if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    console.log(
+      "Warning - request header X-Hub-Signature not present or invalid"
+    );
     res.sendStatus(401);
     return;
   }
-  if (req.body.object) {
-    if (
-      req.body.entry &&
-      req.body.entry[0].changes &&
-      req.body.entry[0].changes[0] &&
-      req.body.entry[0].changes[0].value.messages &&
-      req.body.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+ console.log("request header X-Hub-Signature validated");
+ let body = req.body
+ webhook = JSON.parse(body.replace(/\<pre\>|\<\/pre\>/gm, ''))
 
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      axios({
-        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-        url:
-          "https://graph.facebook.com/v12.0/" +
-          phone_number_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: "Ack: " + msg_body },
-        },
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-     received_updates.unshift(req.body);
-     res.sendStatus(200);
-  } else {
-    // Return a '404 Not Found' if event is not from a WhatsApp API
-    res.sendStatus(404);
+ let messsages =[]
+
+ for (const index in webhook) {
+  if (webhook[index].entry[0].changes[0].value.messages) {
+    // Push to array of messageObjects
+    messsages.push(webhook[index]);
   }
+}
 
 
-  console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
+  received_updates.unshift(messsages);
   res.sendStatus(200);
 });
 
